e039a3821132dda52a5a1dd592637b832b0d4bd5
Benjamin <benjamin@benjamind.local>
2018-03-29 18:01:22 +0800

    * make category folders. * update create certs script

---
+++ ssl_script/create_certs.sh
@@ -0,0 +1,7 @@
+# !/usr/bin 
+
+OUTPUT_KEY="/etc/ssl/private/selfsigned.key"
+OUTPUT_CRT="/etc/ssl/certs/selfsigned.crt"
+
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $OUTPUT_KEY -out $OUTPUT_CRT
+
